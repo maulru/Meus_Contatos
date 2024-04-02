4612ffe1d5feca0ae2377ad3e3628ca04fa91466
Mauro <maurojr_@outlook.com>
2024-04-01 21:36:01 -0300

    Commit inicial

---
+++ Meus_Contatos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meus_Contatos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
