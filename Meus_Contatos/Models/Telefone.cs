1a3a15a86064921a4c043a860a26d8ae7a9e80fa
Mauro <maurojr_@outlook.com>
2024-05-07 23:00:27 -0300

    Criação dos modelos e instalação de nugets do Entity

---
+++ Meus_Contatos/Models/Telefone.cs
@@ -0,0 +1,13 @@
+﻿namespace Meus_Contatos.Models
+{
+    public class Telefone
+    {
+        public int Id { get; set; }
+
+        public virtual Contato Contato { get; set; }
+
+        public int DDD { get; set; }
+
+        public string NumeroTelefone { get; set; }
+    }
+}
---
+++ Meus_Contatos/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Meus_Contatos.Models
+{
+    public class Usuario : IdentityUser
+    {
+        public Usuario() : base() { }
+    }
+}
