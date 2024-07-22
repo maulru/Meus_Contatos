659a2374f58e832bd0b63f3a4629bbc8d4333d33
Mauro Alves Jr <31009363+maulru@users.noreply.github.com>
2024-07-21 22:42:57 -0300

    Comentários pendentes Contato Repository

--- Core/Repository/IContatoRepository.cs
+++ Core/Repository/IContatoRepository.cs
@@ -5,7 +5,6 @@ namespace Core.Repository
     public interface IContatoRepository : IRepository<Contato>
     {
         IEnumerable<Contato> ObterTodos();
-        Contato ObterInformacoesPorId(int id);
         public Contato ObterPorId(int id);
         Contato Cadastrar(Contato contato);
         IEnumerable<Contato> BuscarContatosPorDDD(string ddd);
